8f2b3babff508427c985f0bf6ce7af8d363cbf7a
Mateus Knelsen <sapientia_@hotmail.com>
2024-10-03 23:23:26 +0200

    Add Logo component

---
+++ app/components/Logo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <NuxtImg
+    :src="'logos/vom-prater-aus.svg'"
+    alt="Vom Prater Aus logo"
+    class="vpa-logo"
+  />
+</template>
+
+<style scoped>
+  .vpa-logo {
+    width: 100%;
+    max-width: 100px;
+    height: auto;
+  }
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,7 @@
 
 <template>
   <div>
+    <Logo />
     <BaseButton
       label="Test label"
       type="secondary"
