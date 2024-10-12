e109d42e1415492dc255fc057a30f2f20e8461c6
Mateus Knelsen <sapientia_@hotmail.com>
2024-10-12 12:02:54 +0200

    Add vignette to Background component

--- app/components/Background.vue
+++ app/components/Background.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
+  <div class="vignette" />
   <div class="overlay" />
   <div class="texture" />
 </template>
@@ -10,7 +11,7 @@
     position: fixed;
     top: 0;
     left: 0;
-    z-index: -2;
+    z-index: -3;
     width: 100%;
     height: 100%;
     background: url('@/assets/svgs/noise.svg');
@@ -21,10 +22,26 @@
     position: fixed;
     top: 0;
     left: 0;
-    z-index: -1;
+    z-index: -2;
     width: 100%;
     height: 100%;
     background: var(--light-beige);
     mix-blend-mode: multiply;
+  }
+
+  .vignette {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    box-shadow:
+      inset 10px 20px 300px 0 var(--brown),
+      inset -10px -20px 300px 0 var(--brown),
+      inset 20px -10px 300px 0 var(--brown),
+      inset -20px 10px 300px 0 var(--brown);
+    mix-blend-mode: color-burn;
+    opacity: 0.2;
   }
 </style>
