911e16857c6b67429a548cdc3c091112c4a0acbb
Mateus Knelsen <sapientia_@hotmail.com>
2024-10-12 12:04:48 +0200

    Update temp index view

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <Logo />
+  <div class="content-wrapper">
     <BaseButton
       label="Test label"
       type="secondary"
@@ -13,7 +12,22 @@
       type="secondary"
       variant="icon"
     />
+    <h1>Example heading 1</h1>
+    <h2>Example heading 2</h2>
+    <h3>Example heading 3</h3>
+    <h4>Example heading 4</h4>
+    <p>Example paragraph, lorem ipsum et sit amet.</p>
+    <a href="">Example link</a>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .content-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+  }
+</style>
