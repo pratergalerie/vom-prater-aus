15bec4bd7d840c4ae108050f2708db64acf0e6cc
Mateus Knelsen <sapientia_@hotmail.com>
2024-10-12 12:04:00 +0200

    Add logotype to Logo component

--- app/components/Logo.vue
+++ app/components/Logo.vue
@@ -1,17 +1,53 @@
 <script setup lang="ts"></script>
 
 <template>
-  <NuxtImg
-    :src="'logos/vom-prater-aus.svg'"
-    alt="Vom Prater Aus logo"
-    class="vpa-logo"
-  />
+  <div class="vpa-logo">
+    <NuxtImg
+      :src="'logos/vom-prater-aus.svg'"
+      alt="Vom Prater Aus logo"
+      class="logo"
+    />
+    <div class="logotype">
+      <span>Vom</span>
+      <span>Prater</span>
+      <span>Aus</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
   .vpa-logo {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 150px;
+    height: auto;
+  }
+
+  .logo {
     width: 100%;
     max-width: 100px;
     height: auto;
+  }
+
+  .logotype {
+    display: flex;
+    flex-direction: column;
+    align-self: flex-end;
+    justify-content: center;
+    width: 56%;
+    font-family: Helvetica, sans-serif;
+    font-size: 1.2rem;
+    font-weight: 700;
+    line-height: 1.1rem;
+
+    span:nth-child(1) {
+      align-self: flex-end;
+      padding-right: 1rem;
+    }
+
+    span:nth-child(3) {
+      align-self: flex-end;
+    }
   }
 </style>
