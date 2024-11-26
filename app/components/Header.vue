69cb7a3321dc785ee41a7b9d4965c71b1c676bc2
Mateus Knelsen <mateus.knelsen@iis.fraunhofer.de>
2024-11-26 20:26:38 +0100

    Add logo background image to header component for enhanced visual design

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,25 +1,46 @@
 <script setup lang="ts">
   const { toggleMenu } = useMenuStore()
+
+  const logoBackgroundImgSrc = '/svgs/logo-background.svg'
 </script>
 
 <template>
   <header>
     <Logo @click="toggleMenu" />
+    <div class="logo-background">
+      <img
+        :src="logoBackgroundImgSrc"
+        alt="Logo background"
+      />
+    </div>
   </header>
 </template>
 
 <style scoped>
   header {
+    position: relative;
     box-sizing: border-box;
     width: 100%;
     height: var(--header-height);
     padding: var(--padding);
+  }
 
-    /* Gradient background, from top to bottom */
-    background: linear-gradient(
-      180deg,
-      rgb(244 238 217 / 80%) 0%,
-      rgb(244 238 217 / 0%) 100%
-    );
+  .logo-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 50%;
+    height: 100%;
+
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: -1;
+      width: auto;
+      height: 100%;
+      object-fit: cover;
+    }
   }
 </style>
