6b553befb95733e0741b9b310a6db118af1cd8af
Mateus Knelsen <mateus.knelsen@iis.fraunhofer.de>
2024-11-26 20:25:21 +0100

    Remove console log from menu toggle function for cleaner output

--- app/stores/menu.ts
+++ app/stores/menu.ts
@@ -1,8 +1,8 @@
 export const useMenuStore = defineStore('menu', () => {
   const isOpen = ref(false)
+
   function toggleMenu() {
     isOpen.value = !isOpen.value
-    console.log('Menu is open:', isOpen.value)
   }
 
   return { isOpen, toggleMenu }
