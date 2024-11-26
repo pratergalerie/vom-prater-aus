e17695de5bce68c720522c2dd00b2ac18f300118
Mateus Knelsen <mateus.knelsen@iis.fraunhofer.de>
2024-11-26 20:27:31 +0100

    Implement body scroll lock when menu is open and add fade transition for main content

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
   const { isOpen } = storeToRefs(useMenuStore())
+
+  // Setup body scroll lock when menu is open
+  watch(isOpen, (open) => {
+    if (open) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = ''
+    }
+  })
 </script>
 
 <template>
@@ -12,9 +21,14 @@
         class="menu"
       />
     </Transition>
-    <main>
-      <slot />
-    </main>
+    <Transition name="fade">
+      <main
+        v-if="!isOpen"
+        class="main"
+      >
+        <slot />
+      </main>
+    </Transition>
   </div>
 </template>
 
@@ -54,5 +68,21 @@
   .slide-enter-to,
   .slide-leave-from {
     transform: translateX(0);
+  }
+
+  /* fade */
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-to,
+  .fade-leave-from {
+    opacity: 1;
   }
 </style>
