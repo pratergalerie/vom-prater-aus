c710b268b105edefba483fd41a4c6f23f0ede5d7
Mateus Knelsen <mateus.knelsen@iis.fraunhofer.de>
2024-11-26 20:27:01 +0100

    Refactor Logo component by replacing logotype with SVG

--- app/components/Logo.vue
+++ app/components/Logo.vue
@@ -7,11 +7,6 @@
       alt="Vom Prater Aus logo"
       class="logo-image"
     />
-    <div class="logotype">
-      <span>Vom</span>
-      <span>Prater</span>
-      <span>Aus</span>
-    </div>
   </button>
 </template>
 
@@ -26,36 +21,12 @@
     margin: 0;
     cursor: pointer;
     background: none;
-
-    /* Add drop shadow */
-    filter: drop-shadow(0 1rem 1rem rgb(244 238 217 / 100%));
     border: 0;
   }
 
   .logo-image {
-    width: 100%;
-    max-width: 60%;
-    height: auto;
-  }
-
-  .logotype {
-    display: flex;
-    flex-direction: column;
-    align-self: flex-end;
-    justify-content: center;
-    width: 56%;
-    font-family: Helvetica, sans-serif;
-    font-size: 1rem;
-    font-weight: 700;
-    line-height: 1.1rem;
-
-    span:nth-child(1) {
-      align-self: flex-end;
-      padding-right: 1rem;
-    }
-
-    span:nth-child(3) {
-      align-self: flex-end;
-    }
+    width: auto;
+    height: 100%;
+    object-fit: contain;
   }
 </style>
