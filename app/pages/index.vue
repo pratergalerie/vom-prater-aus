c7f24d3ee63f92ad8e9016ffebdc4554a9bc392c
Mateus Knelsen <mateus.knelsen@iis.fraunhofer.de>
2024-11-26 22:27:49 +0100

    Add sections for stories, prater, and create in index page, add initial localized content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,46 +2,48 @@
 
 <template>
   <div class="content-wrapper">
-    <BaseButton
-      label="Test label"
-      type="secondary"
-      variant="label"
-    />
-    <BaseButton
-      icon="account"
-      type="secondary"
-      variant="icon"
-    />
-    <h1>Example heading 1</h1>
-    <h2>Example heading 2</h2>
-    <h3>Example heading 3</h3>
-    <h4>Example heading 4</h4>
-    <p>Example paragraph, lorem ipsum et sit amet.</p>
-    <a href="">Example link</a>
-    <h1>Example heading 1</h1>
-    <h2>Example heading 2</h2>
-    <h3>Example heading 3</h3>
-    <h4>Example heading 4</h4>
-    <p>Example paragraph, lorem ipsum et sit amet.</p>
-    <a href="">Example link</a>
-    <h1>Example heading 1</h1>
-    <h2>Example heading 2</h2>
-    <h3>Example heading 3</h3>
-    <h4>Example heading 4</h4>
-    <p>Example paragraph, lorem ipsum et sit amet.</p>
-    <a href="">Example link</a>
-    <h1>Example heading 1</h1>
-    <h2>Example heading 2</h2>
-    <h3>Example heading 3</h3>
-    <h4>Example heading 4</h4>
-    <p>Example paragraph, lorem ipsum et sit amet.</p>
-    <a href="">Example link</a>
-    <h1>Example heading 1</h1>
-    <h2>Example heading 2</h2>
-    <h3>Example heading 3</h3>
-    <h4>Example heading 4</h4>
-    <p>Example paragraph, lorem ipsum et sit amet.</p>
-    <a href="">Example link</a>
+    <section class="section stories">
+      <div class="text-block stories">
+        <h1>{{ $t('pages.home.sections.stories.title') }}</h1>
+        <p>
+          {{ $t('pages.home.sections.stories.text') }}
+        </p>
+      </div>
+      <BaseButton
+        :label="$t('pages.home.sections.stories.button')"
+        icon="arrow-right"
+        type="primary"
+        variant="label-icon"
+      />
+    </section>
+    <section class="section prater">
+      <div class="text-block prater">
+        <h1>{{ $t('pages.home.sections.prater.title') }}</h1>
+        <p>
+          {{ $t('pages.home.sections.prater.text') }}
+        </p>
+      </div>
+      <BaseButton
+        :label="$t('pages.home.sections.prater.button')"
+        icon="arrow-right"
+        type="primary"
+        variant="label-icon"
+      />
+    </section>
+    <section class="section create">
+      <div class="text-block create">
+        <h1>{{ $t('pages.home.sections.create.title') }}</h1>
+        <p>
+          {{ $t('pages.home.sections.create.text') }}
+        </p>
+      </div>
+      <BaseButton
+        :label="$t('pages.home.sections.create.button')"
+        icon="arrow-right"
+        type="primary"
+        variant="label-icon"
+      />
+    </section>
   </div>
 </template>
 
@@ -49,9 +51,54 @@
   .content-wrapper {
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    gap: 300px;
     justify-content: center;
     width: 100%;
     height: 100%;
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+
+  .text-block {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin: 0;
+
+    h1 {
+      font-size: 1.4rem;
+      line-height: 1.8rem;
+    }
+
+    p {
+      font-size: 1rem;
+      line-height: 1.3rem;
+    }
+
+    &.stories {
+      max-width: 80%;
+    }
+
+    &.prater {
+      align-self: flex-end;
+      max-width: 80%;
+    }
+
+    &.create {
+      align-self: flex-end;
+      max-width: 80%;
+
+      h1,
+      p {
+        text-align: right;
+      }
+    }
   }
 </style>
