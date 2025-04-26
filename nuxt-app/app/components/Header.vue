4e62f6b819d2a19c827a39db7669557c51f66bf7
Mateus Knelsen <sapientia_@hotmail.com>
2025-04-26 20:07:53 +0200

    Enhance Header component styles by adding a background color and a mask for improved visual aesthetics. Introduce a noise texture overlay for a more dynamic appearance, ensuring the header maintains its structure and responsiveness.

--- nuxt-app/app/components/Header.vue
+++ nuxt-app/app/components/Header.vue
@@ -14,10 +14,24 @@
     z-index: 99;
     box-sizing: border-box;
     width: 100%;
-    max-width: 1000px;
     height: var(--header-height);
     padding: var(--padding-mobile);
     margin: 0 auto;
+    background-color: var(--color-beige);
+    mask: url('/svgs/header/mask.svg');
+    mask-repeat: no-repeat;
+    mask-composite: exclude;
+
+    &::before {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      content: '';
+      background: url('@/assets/svgs/noise.svg');
+      mix-blend-mode: multiply;
+    }
 
     @media (min-width: 768px) {
       left: 50%;
