5d87d44a57c95a6afc72ba7b7a40a5da8c1786dd
Erik Meseck <git@meseck.dev>
2025-10-24 08:28:47 +0200

    Configure brevo for emails in production

---
+++ strapi/config/env/production/plugins.ts
@@ -0,0 +1,15 @@
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "strapi-provider-email-brevo",
+      providerOptions: {
+        apiKey: env.BREVO_API_KEY,
+      },
+      settings: {
+        defaultSenderEmail: "vomprateraus@pratergalerie.de",
+        defaultSenderName: "Vom Prater Aus",
+        defaultReplyTo: "vomprateraus@pratergalerie.de",
+      },
+    },
+  },
+});
