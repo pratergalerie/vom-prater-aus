2869bcb74eb15a9abddeb74a428966869a513354
Erik Meseck <git@meseck.dev>
2025-10-30 09:09:21 +0100

    Use drop-shadow

--- nuxt-app/app/components/base/Input.vue
+++ nuxt-app/app/components/base/Input.vue
@@ -49,7 +49,6 @@
         />
 
         <div class="text-input foreground" />
-        <div class="text-input background" />
       </div>
     </label>
 
@@ -83,6 +82,7 @@
     position: relative;
     display: grid;
     grid-template: 'stack' 1fr / 1fr;
+    filter: drop-shadow(-6px 6px 0 var(--color-black));
   }
 
   label {
@@ -137,33 +137,6 @@
     z-index: 2;
     grid-area: stack;
     background-color: var(--color-white);
-    clip-path: polygon(
-      0% 10%,
-      12.22% 5.3%,
-      21.5% 10.87%,
-      53.76% 8.25%,
-      98.35% 0%,
-      98.64% 76.25%,
-      97.26% 89.26%,
-      88.2% 82.2%,
-      83.73% 91.73%,
-      70.85% 94.85%,
-      61.69% 78.69%,
-      53.76% 93.76%,
-      26.98% 92.98%,
-      1.26% 89.26%,
-      0% 73.2%,
-      0% 100%
-    );
-  }
-
-  .background {
-    position: relative;
-    top: 5px;
-    left: -5px;
-    z-index: 1;
-    grid-area: stack;
-    background-color: var(--color-black);
     clip-path: polygon(
       0% 10%,
       12.22% 5.3%,
