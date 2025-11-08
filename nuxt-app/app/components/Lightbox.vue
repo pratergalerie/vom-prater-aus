0ea1cb872ba3113d76e5bcb43b3136e6873978ba
Erik Meseck <git@meseck.dev>
2025-11-08 17:13:59 +0100

    Revert latest changes

--- nuxt-app/app/components/Lightbox.vue
+++ nuxt-app/app/components/Lightbox.vue
@@ -102,8 +102,8 @@
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 100dvh;
-    height: 100dvh;
+    width: 100%;
+    height: 100%;
     padding: 0;
     margin: 0;
     cursor: pointer;
